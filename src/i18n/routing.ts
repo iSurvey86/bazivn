b2f4bf174db2790a28e5214c17930a91777a57e6
iSurvey86 <minhphuong.npsc@gmail.com>
2026-07-03 00:33:05 +0700

    Default locale vn: disable browser locale detection

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,4 +3,5 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["vn", "en", "zh", "ko"],
   defaultLocale: "vn",
+  localeDetection: false,
 });
